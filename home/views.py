59c0158e8f4256d306c4894933b25277e428680f
zaidsidd360 <zaidsidd360@gmail.com>
2022-09-02 22:03:41 +0530

    added contact form backend

--- home/views.py
+++ home/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, HttpResponse
+from datetime import datetime
+from home.models import Contact
+from django.contrib import messages
 
 # Create your views here.
 
@@ -23,4 +26,12 @@ def services(request):
 
 def contact(request):
     # return HttpResponse("This is contact page")
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        contact = Contact(name=name, email=email,
+                          message=message, date=datetime.today())
+        contact.save()
+        messages.success(request, 'Thanks for contacting us!')
     return render(request, 'contact.html')
